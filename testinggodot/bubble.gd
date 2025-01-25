a7961bf14047c6b960cb33fef6dc54dcb07b083c
callousmuppet <callousmuppet@gmail.com>
2025-01-25 01:16:58 -0500

    shooting bubbles

---
+++ testinggodot/bubble.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+var velocity: Vector2 = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float):
+	position += velocity * delta
+	
+	if not is_on_screen():
+		print("bubble remove")
+		queue_free()
+
+func set_velocity(new_velocity: Vector2):
+	velocity = new_velocity
+
+func get_bubble_size() -> Vector2:
+	var sprite: Sprite2D = $Sprite2D
+	if sprite and sprite.texture:
+		return sprite.texture.get_size() * sprite.scale
+	return Vector2.ZERO
+		
+func is_on_screen():
+	var viewport = get_viewport().get_visible_rect()
+	var bubble_size = get_bubble_size()
+	viewport.size += bubble_size
+	viewport.position -= bubble_size / 2
+	return viewport.has_point(global_position)
--- testinggodot/cleaner.gd
+++ testinggodot/cleaner.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
+@export var Bubble: PackedScene
+
 var speed: float = 500.0
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:	
+func _process(delta: float):	
 	var direction: Vector2 = Vector2.ZERO
 	
 	if Input.is_action_pressed("up"):
@@ -30,4 +32,27 @@ func _process(delta: float) -> void:
 		move_and_slide()
 		look_at(global_position + direction)
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("bubble"):
+		shoot()
 	
+func shoot():
+	create_bubble()
+
+func create_bubble():
+	if Bubble:
+		var bubble_inst = Bubble.instantiate()
+		get_parent().add_child(bubble_inst)
+		
+		var direction = velocity.normalized()
+		if direction == Vector2.ZERO:
+			direction = Vector2.RIGHT
+		
+		bubble_inst.global_position = global_position + direction * 120
+		
+		if bubble_inst.has_method("set_velocity"):
+			bubble_inst.set_velocity(direction * 200)
+		else:
+			print("Bubble does not have set_velocity")
+	else:
+		print("Bubble scene not assigned")
