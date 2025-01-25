240a26b7207672493d36fa9254ac2db56e481d6b
Ryan Morrison-Wesphal <mr.tyzik@gmail.com>
2025-01-25 14:50:07 -0500

    dust bunny spitting out dust

--- testinggodot/dust_bunny.gd
+++ testinggodot/dust_bunny.gd
@@ -6,6 +6,8 @@ class_name DustBunny
 
 var health: int = 100
 
+var dust_angle: float = 0.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -20,15 +22,24 @@ func bubble_process():
 	if health <= 0:
 		queue_free()
 
-
 func _on_dust_timer_timeout() -> void:
-	create_dust()
+	create_dust(dust_angle)
+	dust_angle += 45.0
 	
-func create_dust():
-	print("dust_bunny create_dust")
+func create_dust(angle_degrees: float = 0.0, speed: float = 200.0):
 	if DustParticle:
 		var dust_inst = DustParticle.instantiate()
-		add_child(dust_inst)
-		dust_inst.global_position = global_position
+		get_parent().add_child(dust_inst)
+		
+		var angle_radians = deg_to_rad(angle_degrees)
+		
+		var offset = Vector2(cos(angle_radians), sin(angle_radians)) * 50
+		dust_inst.global_position = global_position + offset
+
+		if dust_inst.has_method("set_velocity"):
+			var velocity = Vector2(cos(angle_radians), sin(angle_radians)) * speed
+			dust_inst.set_velocity(velocity)
+		else:
+			print("DustParticle doesn't have a 'set_velocity' method")
 	else:
 		print("DustParticle scene not assigned")
--- testinggodot/dust_particle.gd
+++ testinggodot/dust_particle.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 class_name DustParticle
 
+var velocity: Vector2 = Vector2.ZERO
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,9 +11,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta: float):
+	position += velocity * delta
 
 func bubble_process():
 	queue_free()
 
-func _on_body_entered(body: Node2D) -> void:
-	queue_free()
+func _on_body_entered(body: Node2D):
+	# don't disappear if touching a DustBunny
+	if not body.has_method("create_dust"):
+		queue_free()
+
+func set_velocity(new_velocity: Vector2):
+	velocity = new_velocity
