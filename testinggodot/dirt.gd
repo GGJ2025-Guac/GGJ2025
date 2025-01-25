0decb313a0c3db85c348f84e6bcd3b97580a8458
utkarshdev2 <awesomeutk@gmail.com>
2025-01-25 10:25:53 -0500

    Push 1
    Camera following player
    Camera zoomed out
    Enemy movement (beta)

--- testinggodot/dirt.gd
+++ testinggodot/dirt.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
+@onready var player = get_parent().get_node("Cleaner")
+@export var speed : float = 500
 
+func _physics_process(delta : float):
+	var player_position = player.position
+	var target_position = (player_position - position).normalized()
+	
+	move_and_slide()
+	look_at(player_position)
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
