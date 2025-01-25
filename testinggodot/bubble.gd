f3bc6950ce9ade534f5c9ae86f47e62a12c0c581
Ryan Morrison-Wesphal <mr.tyzik@gmail.com>
2025-01-25 12:34:29 -0500

    add dust particle
    Hook up dust particle as area

--- testinggodot/bubble.gd
+++ testinggodot/bubble.gd
@@ -23,6 +23,12 @@ func _on_pop_timer_timeout() -> void:
 	queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.has_method("bubble_process"):
-		body.bubble_process()
-		queue_free()
+	node_entered(body)
+
+func _on_area_entered(area: Area2D) -> void:
+	node_entered(area)
+	
+func node_entered(node):
+	if node.has_method("bubble_process"):
+		node.bubble_process()
+		queue_free()	
