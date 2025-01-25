5c03e85440a05b3175756e0f6055458ea0956c1c
Ryan Morrison-Wesphal <mr.tyzik@gmail.com>
2025-01-25 14:11:43 -0500

    Attempt to create dust particles
    Having issues, DustParticle isn't assigned even though it's a PackedScene

--- testinggodot/dust_bunny.gd
+++ testinggodot/dust_bunny.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 class_name DustBunny
 
+@export var DustParticle: PackedScene
+
 var health: int = 100
 
 # Called when the node enters the scene tree for the first time.
@@ -17,3 +19,16 @@ func bubble_process():
 	health -= 34
 	if health <= 0:
 		queue_free()
+
+
+func _on_dust_timer_timeout() -> void:
+	create_dust()
+	
+func create_dust():
+	print("dust_bunny create_dust")
+	if DustParticle:
+		var dust_inst = DustParticle.instantiate()
+		add_child(dust_inst)
+		dust_inst.global_position = global_position
+	else:
+		print("DustParticle scene not assigned")
