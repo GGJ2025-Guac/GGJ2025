f5bc4c6c6ff9b681293b1df7181fe35ed45348da
Ryan Morrison-Wesphal <mr.tyzik@gmail.com>
2025-01-26 11:17:58 -0500

    Revert: Revert: Push

--- testinggodot/bubble.gd
+++ testinggodot/bubble.gd
@@ -1,25 +1,19 @@
-extends Area2D
+extends CharacterBody2D
 
 class_name Bubble
 
-signal play_pop()
+var pos: Vector2
+var rot: float
+var direction: float
+var speed: float = 400
 
-var velocity: Vector2 = Vector2.ZERO
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func _physics_process(delta: float):
-	position += velocity * delta
-
-func set_velocity(new_velocity: Vector2):
-	velocity = new_velocity
+	global_position = pos
+	global_rotation = rot
+	
+func _physics_process(delta: float) -> void:
+	velocity = Vector2(speed, 0).rotated(direction)
+	move_and_slide()
 
 func _on_pop_timer_timeout() -> void:
 	pop_bubble()
@@ -33,8 +27,4 @@ func _on_area_entered(area: Area2D) -> void:
 func node_entered(node):
 	if node.has_method("bubble_process"):
 		node.bubble_process()
-		pop_bubble()
-
-func pop_bubble():
-	emit_signal("play_pop")
-	queue_free()
+		queue_free()
