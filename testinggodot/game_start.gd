250ceb94429ed808de821ae2719d068238ee1a6d
Ryan Morrison-Wesphal <mr.tyzik@gmail.com>
2025-01-26 03:36:46 -0500

    some more controller support

--- testinggodot/game_start.gd
+++ testinggodot/game_start.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+@onready var start_button: Button = $Start
+@onready var end_button: Button = $Quit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,10 +12,17 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
 
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_start"):
+		start_button.emit_signal("pressed")
+	
+	if event.is_action_pressed("ui_back"):
+		end_button.emit_signal("pressed")
+		
+		
--- testinggodot/gameover.gd
+++ testinggodot/gameover.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+@onready var start_button: Button = $Retry
+@onready var end_button: Button = $Quit
 
 func _on_retry_pressed() -> void:
 	get_tree().change_scene_to_file("res://main.tscn")
@@ -7,3 +9,10 @@ func _on_retry_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_start"):
+		start_button.emit_signal("pressed")
+	
+	if event.is_action_pressed("ui_back"):
+		end_button.emit_signal("pressed")
